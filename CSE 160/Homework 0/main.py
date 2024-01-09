b208e34e545164c4d7f15cbf26f4e34308fd3df6
Ananya Shreya Soni <76674905+ananyasoni@users.noreply.github.com>
2024-01-08 19:24:04 -0800

    Add files via upload

---
+++ CSE 160/Homework 0/main.py
@@ -0,0 +1,4 @@
+# Problem 1:
+# Replace XXX with your answer.
+print("first is 9")
+print("second is 8")
---
+++ CSE 160/Homework 0/variables.py
@@ -0,0 +1,8 @@
+# Problem 2:
+# a. Assign the value 160 into a variable
+value1 = 160
+# b. Multiply the value in the variable by 2 and then
+#    store it back into the same variable
+value1 = value1 * 2
+# c. Print out the variable
+print(value1)
