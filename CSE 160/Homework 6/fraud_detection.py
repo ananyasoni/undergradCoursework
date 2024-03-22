a2ee4ab336a86ae16b63b12a66dba93609f2f8db
Ananya Shreya Soni <76674905+ananyasoni@users.noreply.github.com>
2024-03-21 18:18:43 -0700

    Update fraud_detection.py

--- CSE 160/Homework 6/fraud_detection.py
+++ CSE 160/Homework 6/fraud_detection.py
@@ -7,7 +7,6 @@ This program analyzes Iran election data to determine
 whether the election was likely fraudelent or not.
 """
 import utils  # noqa: F401, do not remove if using a Mac
-# add your imports BELOW this line
 import csv
 import random
 import matplotlib.pyplot as plt
@@ -22,17 +21,6 @@ def ones_and_tens_digit_histogram(numbers):
         appeared in the ones place OR the tens place in the input list. This
         returned list will always have 10 numbers (representing the frequency
         of digits 0 - 9).
-
-    For example, given the input list
-        [127, 426, 28, 9, 90]
-    This function will return
-        [0.2, 0.0, 0.3, 0.0, 0.0, 0.0, 0.1, 0.1, 0.1, 0.2]
-
-    That is, the digit 0 occurred in 20% of the one and tens places; 2 in 30%
-    of them; 6, 7, and 8 each in 10% of the ones and tens, and 9 occurred in
-    20% of the ones and tens.
-
-    See fraud_detection_tests.py for additional cases.
     '''
     histogram = [0] * 10
 
